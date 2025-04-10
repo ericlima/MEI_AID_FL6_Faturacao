d3fb49ee7dc1a0da234d3be39bfcd9ed9da89201
Eric Lima <eric.george.lima@gmail.com>
2025-04-10 06:12:46 +0100

    initial

---
+++ src/main/java/pt/ipcb/ad/faturacao/FaturacaoApplication.java
@@ -0,0 +1,13 @@
+package pt.ipcb.ad.faturacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaturacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pt/ipcb/ad/faturacao/FaturacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ipcb.ad.faturacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
